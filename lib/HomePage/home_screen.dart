e4c1d946d17dcf14eb4583916f1f8d4734a936cc
hamidhosen42 <mdhamidhosen8444@gmail.com>
2023-01-10 19:40:48 +0600

    google map multiple marker

--- lib/HomePage/home_screen.dart
+++ lib/HomePage/home_screen.dart
@@ -17,39 +17,80 @@ class _HomeScreenState extends State<HomeScreen> {
       Completer<GoogleMapController>();
 
   List<Marker> _markers = <Marker>[];
+  final List<Marker> _list = [
+    Marker(
+        markerId: MarkerId('1'),
+        position: LatLng(22.341900, 91.815536),
+        infoWindow: InfoWindow(title: 'My Current Location')),
+    Marker(
+        markerId: MarkerId('2'),
+        position: LatLng(22.341900, 91.9092),
+        infoWindow: InfoWindow(title: 'Another Location-1')),
+    Marker(
+        markerId: MarkerId('3'),
+        position: LatLng(22.341900, 91.015536),
+        infoWindow: InfoWindow(title: 'Another Location-2')),
+  ];
 
   static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(33.6844, 73.0479),
-    zoom: 14,
+    target: LatLng(22.341900, 91.815536),
+    zoom: 15,
   );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(22.341900, 91.815536),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
 
   @override
   void initState() {
     super.initState();
-    _markers.add(Marker(
-        markerId: MarkerId('SomeId'),
-        position: LatLng(37.42796133580664, -122.085749655962),
-        infoWindow: InfoWindow(title: 'The title of the marker')));
+    _markers.addAll(_list);
   }
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   _markers.add(Marker(
+  //       markerId: MarkerId('1'),
+  //       position: LatLng(37.42796133580664, -122.085749655962),
+  //       infoWindow: InfoWindow(title: 'My Current Location')));
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: GoogleMap(
-        initialCameraPosition: _kGooglePlex,
-        mapType: MapType.normal,
-        zoomControlsEnabled: true,
-        zoomGesturesEnabled: true,
-        myLocationButtonEnabled: true,
-        myLocationEnabled: true,
-        trafficEnabled: false,
-        rotateGesturesEnabled: true,
-        buildingsEnabled: true,
-        markers: Set<Marker>.of(_markers),
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body: SafeArea(
+        child: GoogleMap(
+          initialCameraPosition: _kGooglePlex,
+          // mapType: MapType.normal,
+          // zoomControlsEnabled: true,
+          // zoomGesturesEnabled: true,
+          // myLocationButtonEnabled: true,
+          // myLocationEnabled: true,
+          // // trafficEnabled: true,
+          // rotateGesturesEnabled: true,
+          // buildingsEnabled: true,
+          markers: Set<Marker>.of(_markers),
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 50),
+        child: FloatingActionButton.extended(
+          onPressed: _goToTheLake,
+          label: const Text('To the lake!'),
+          icon: const Icon(Icons.directions_boat),
+        ),
       ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
